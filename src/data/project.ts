38b343b84b3b8f3758e1c25716677f1f12325fa0
Sakthivelan2005 <sakthivelan.shankar@gmail.com>
2026-03-18 19:13:44 +0530

    Updated Project

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,14 +1,14 @@
 export const projects = [
   {
     title: "MeTime Beauty App",
-    tech: "React Native | Node.js",
+    tech: "React Native | Node.js | Expo",
     description:
       "Mobile beauty service booking app with authentication and backend integration.",
     github: "https://github.com/Sakthivelan2005/MeTime",
   },
   {
     title: "MERN E-commerce",
-    tech: "MongoDB | Express | React | Node",
+    tech: "MongoDB | Express.js | React.js | Node.js",
     description:
       "Full-stack e-commerce application with order management system.",
     github: "https://github.com/Sakthivelan2005/e-commerce",
@@ -18,6 +18,13 @@ export const projects = [
     tech: "HTML | CSS | JavaScript | Responsive UI",
     description:
       "Real estate marketplace platform with optimized user interface.",
+    github: "https://github.com/Sakthivelan2005/Real_Estate_Platform",
+  },
+   {
+    title: "Tailor Junction",
+    tech: "React Native | MySQL | Express.js | Node.js | Socket.io | Expo",
+    description:
+      "Tailor Junction is an Uber-style, real-time marketplace empowering MSME tailors. Engineered with React Native, Express.js Node.js, and Socket.io for live algorithmic order broadcasting and system telemetry.",
     github: "https://github.com/Sakthivelan2005/Real_Estate_Platform",
   },
 ];
